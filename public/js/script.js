b98029e1749661103df2b3546cc5ade13bb5f533
[GitCeres] <[maisousuisje@gmail.com]>
2022-10-05 12:40:14 +0200

    CREATE navbar

---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+const hamburgerToggler = document.querySelector(".hamburger");
+const navLinksContainerSlide = document.querySelector(".navlinks-container-slide");
+const shadowBody = document.body;
+
+// Affiche ou cache la side bar et le fond sombre au format mobile
+function toggleNav(){
+    hamburgerToggler.classList.toggle("open");
+    navLinksContainerSlide.classList.toggle("open");
+    shadowBody.classList.toggle("activate")
+}
+
+hamburgerToggler.addEventListener("click", toggleNav)
+
+// Vérifie la taille de la fenêtre et en fonction ajoute ou non la transition
+new ResizeObserver(entries => {
+    if(entries[0].contentRect.width <= 768){
+        navLinksContainerSlide.style.transition = "transform 0.3s ease-out"
+    } else {
+        navLinksContainerSlide.style.transition = "none"
+    }
+}).observe(document.body)
